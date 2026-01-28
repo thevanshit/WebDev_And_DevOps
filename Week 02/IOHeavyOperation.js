b0db42c209c885c82b6e0992542d670ba9c410f4
thevanshit <vanshit2509@gmail.com>
2026-01-28 16:30:27 +0530

    IO Heavy Operartion using fs system in Js

---
+++ Week 02/IOHeavyOperation.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+// Import the fs(File System) library of Java Script.
+const contents = fs.readFileSync("a.txt", "utf-8"); // Objects
+console.log(contents);
+
+// Let's Make an Object
+
+var User = {
+    age : 20,
+    name : "Vanshit",
+    calculateSum : function(a, b){
+        return a + b;
+    }
+}
+
+console.log(User.calculateSum(12, 88)); // Calling Object 
