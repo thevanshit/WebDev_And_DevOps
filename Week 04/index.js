64a11369d707536f46f7e08cc0f5191938b30123
thevanshit <vanshit2509@gmail.com>
2026-02-02 17:28:43 +0530

    learning about the node cli commands

--- Week 04/index.js
+++ Week 04/index.js
@@ -7,3 +7,35 @@ function multiply(a, b){
 }
 
 console.log(sum(15, 85));
+
+const thenable = {
+    then : function(Onfulfilled){
+        setTimeout(() => Onfulfilled(42), 1000);
+    }
+}
+
+const p = new Promise(function(){});
+console.log(p);
+
+console.log(thenable);
+async function main() {
+    const v = await thenable.then(function(){
+        console.log("Hi There!");
+    });
+}
+main();
+
+const fs = require('fs');
+function master(filename){
+    fs.readFile(filename, 'utf-8', function(err, data){
+        let total = 0;
+        for(let i = 0; i < data.length; i++){
+            if (data[i] == " "){
+                total++;
+            }
+        }
+        console.log(total);
+    });
+}
+
+master('a.txt');
